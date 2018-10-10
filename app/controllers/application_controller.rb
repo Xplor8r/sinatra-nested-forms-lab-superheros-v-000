2e11e387ebb13c0b8d8d39fa7e2abae29a9aa389
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-10 17:29:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-    require './environment'
-    
+    require './config/environment'
+
     get '/' do
       erb :super_hero
     end
-    
+
     post '/teams' do
       @team = Team.new(params[:team])
         params[:team][:members].each do |details|
           Hero.new(details)
         end
         @members = Hero.all
-    
+
       erb :'/team'
     end
 
