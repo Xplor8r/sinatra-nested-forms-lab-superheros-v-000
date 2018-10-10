8ab43f53f74e3ab355f0cfa8dc6d8f6b99ac3429
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-10 17:15:46 +0000

    automatically backed up by learn

--- views/teams.rb
+++ views/teams.rb
@@ -1,13 +1,15 @@
 <h1>Create a Team and Heroes</h1>
-<form action="/pirates" method="post">
-  <p>Team Name: <input type="text" name="pirate[name]"></p>
-  <p>Team Weight: <input type="text" name="pirate[weight]"></p>
-  <p>Team Height: <input type="text" name="pirate[height]"></p>
-  <p>Hero's 1 Name:<input id="member1_name" type="text" name="team[members][][name]"></p>
-  <p>Hero's 1 Type:<input id="ship_type_1" type="text" name="pirate[ships][][type]" ></p>
-  <p>Hero's 1 booty:<input id="ship_booty_1" type="text" name="pirate[ships][][booty]" ></p>
-  <p>Hero's 2 Name:<input id="ship_name_2" type="text" name="pirate[ships][][name]" ></p>
-  <p>Hero's 2 type:<input id="ship_type_2" type="text" name="pirate[ships][][type]" ></p>
-  <p>Hero's 2 booty:<input id="ship_booty_2" type="text" name="pirate[ships][][booty]" ></p>
+<form action="/teams" method="post">
+  <p>Team Name: <input type="text" name="team[name]"></p>
+  <p>Team Motto: <input type="text" name="team[motto]"></p>
+  <h2>Hero 1</h2>
+  <p>Hero&aposs Name:<input id="member1_name" type="text" name="team[members][][name]"></p>
+  <p>Hero&aposs Type:<input id="member1_motto_1" type="text" name="team[members][][motto]" ></p>
+  <p>Hero&aposs Bio:<input id="member1_bio_1" type="text" name="team[members][][bio]" ></p>
+  <h2>Hero 2</h2>
+  <p>Hero&aposs Name:<input id="member1_name_2" type="text" name="team[members][][name]" ></p>
+  <p>Hero&aposs motto:<input id="member1_motto_2" type="text" name="team[members][][motto]" ></p>
+  <p>Hero&aposs bio:<input id="member1_bio_2" type="text" name="team[members][][bio]" ></p>
+  <h2>Hero 1</h2>
   <input type="submit" id="Submit">
 </form>
